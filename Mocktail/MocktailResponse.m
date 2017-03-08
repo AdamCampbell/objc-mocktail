8af9fdf03b25d4a7e8e6c9d0705cb16042ac56b7
Nicholas Cross <nicholas@nacrossweb.com>
2017-03-08 22:00:17 +1100

    Extracted and exposed mocktail response creation.  This allows the file format to be used with other approaches to stubbing out web services for testing, such as registering handlers with GCDWebServer for example.

--- Mocktail/MocktailResponse.m
+++ Mocktail/MocktailResponse.m
@@ -11,5 +11,54 @@
 #import "MocktailResponse.h"
 
 
+@interface MocktailResponse()
+
+@property (nonatomic, strong) NSRegularExpression *methodRegex;
+@property (nonatomic, strong) NSRegularExpression *absoluteURLRegex;
+@property (nonatomic, strong) NSURL *fileURL;
+@property (nonatomic) NSInteger bodyOffset;
+@property (nonatomic, strong) NSDictionary *headers;
+@property (nonatomic) NSInteger statusCode;
+
+@end
+
+
 @implementation MocktailResponse
+
++ (MocktailResponse *)mocktailResponseForFileAtURL:(NSURL *)url {
+    NSAssert(url, @"Expected valid URL.");
+
+    NSError *error;
+    NSStringEncoding originalEncoding;
+    NSString *contentsOfFile = [NSString stringWithContentsOfURL:url usedEncoding:&originalEncoding error:&error];
+    if (error) {
+        NSLog(@"Error opening %@: %@", url, error);
+        return nil;
+    }
+
+    NSScanner *scanner = [NSScanner scannerWithString:contentsOfFile];
+    NSString *headerMatter = nil;
+    [scanner scanUpToString:@"\n\n" intoString:&headerMatter];
+    NSArray *lines = [headerMatter componentsSeparatedByString:@"\n"];
+    if ([lines count] < 4) {
+        NSLog(@"Invalid amount of lines: %u", (unsigned)[lines count]);
+        return nil;
+    }
+
+    MocktailResponse *response = [MocktailResponse new];
+    response.methodRegex = [NSRegularExpression regularExpressionWithPattern:lines[0] options:NSRegularExpressionCaseInsensitive error:nil];
+    response.absoluteURLRegex = [NSRegularExpression regularExpressionWithPattern:lines[1] options:NSRegularExpressionCaseInsensitive error:nil];
+    response.statusCode = [lines[2] integerValue];
+    NSMutableDictionary *headers = [[NSMutableDictionary alloc] init];
+    for (NSString *line in [lines subarrayWithRange:NSMakeRange(3, lines.count - 3)]) {
+        NSArray* parts = [line componentsSeparatedByString:@":"];
+        [headers setObject:[[parts lastObject] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]
+                    forKey:[parts firstObject]];
+    }
+    response.headers = headers;
+    response.fileURL = url;
+    response.bodyOffset = [headerMatter dataUsingEncoding:originalEncoding].length + 2;
+    return response;
+}
+
 @end
